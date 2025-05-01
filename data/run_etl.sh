ae554fbc6f4715941626cf79605346e9d65dd8c0
geethaguruju <geethaguruju@gmail.com>
2025-05-01 00:08:21 -0400

    adding json transformation

--- data/run_etl.sh
+++ data/run_etl.sh
@@ -3,6 +3,9 @@
 echo "Running extract stage..."
 docker compose -f ./data/etl.yaml run extract-data
 
+echo "Running transform stage..."
+docker compose -f ./data/etl.yaml run transform-data
+
 echo "Setting RCLONE_CONTAINER..."
 export RCLONE_CONTAINER=object-persist-project17
 
@@ -11,4 +14,4 @@ docker compose -f ./data/etl.yaml run load-data
 
 echo "Cleaning up Docker volume..."
 docker volume ls
-docker volume rm $(docker volume ls -q --filter name=medicaldata)
+docker volume rm $(docker volume ls -q --filter name=medicaldata) || echo "No volume found to remove."
